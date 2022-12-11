3cf8d257449623d302a725cfb603a3a1d17221ad
Łukasz Jarema <lukasz.jarema@live.com>
2022-12-11 13:45:41 +0100

    Zadanie numer 3 z zadań projektowania algorytmów rozdziału numer 6 rozwiązane.

---
+++ src/com/jarema/lukasz/warsztat/projektanta/algorytmow6/Square.java
@@ -0,0 +1,40 @@
+package com.jarema.lukasz.warsztat.projektanta.algorytmow6;
+
+/**
+ * Rozważ następującą deklarację klasy:
+ * public class Square {
+ *     private double sideLength;
+ *     private double getArea() {
+ *         return sideLength + sideLength;
+ *     }
+ *     public double getSideLength() {
+ *         return sideLength;
+ *     }
+ * }
+ * a). Napisz konstruktor bezargumentowy tej klasy. Powinien on przypisywać wartość 0.0. do pola sideLength.
+ * b). Napisz przeciążony konstruktor tej klasy. Powinien on przyjmować argument kopiowany następnie do pola sideLength.
+ */
+public class Square {
+    private double sideLength;
+    private double getArea() {
+        return sideLength + sideLength;
+    }
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    /**
+     * Konstruktor bezargumentowy.
+     */
+    public Square() {
+        sideLength = 0.0;
+    }
+
+    /**
+     * Konstruktor klasy Square przyjmujący argument.
+     * @param length argument length przypisywany jest do wartości sideLength.
+     */
+    public Square(double length) {
+        length = sideLength;
+    }
+}
