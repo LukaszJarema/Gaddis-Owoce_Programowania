d9047f806a0e9e2cdce97f0898e511671fcf8b17
Łukasz Jarema <lukasz.jarema@live.com>
2022-12-21 16:54:21 +0100

    Utworzenie klasy RolettePocket z informacjami do zadania

---
+++ src/com/jarema/lukasz/zadania/programistyczne/rozdzial6/RoulettePocket.java
@@ -0,0 +1,18 @@
+package com.jarema.lukasz.zadania.programistyczne.rozdzial6;
+
+/**
+ * Kolory w ruletce.
+ * Koło ruletki obejmuje przedziały o numerach od 0 do 36. Oto kolory tych przedziałów:
+ * - Przedział numer 0 jest zielony.
+ * - W zakresie od 1 do 10 przedziały nieparzyste są czerwone, a parzyste czarne.
+ * - W zakresie od 11 do 18 przedziały nieparzyste są czarne, a parzyste są czerwone.
+ * - W zakresie od 19 do 28 przedziały nieparzyste są czerwone, a parzyste czarne.
+ * - W zakresie od 29 do 36 przedziały nieparzyste są czarne, a parzyste czerwone.
+ * Napisz klasę RoulettePocket. Konstruktor tej klasy powinien przyjmować numery przedziału. W klasie należy udostępnić
+ * metodę getPocketColor, która zwraca kolor przedziału jako łańcuch znaków.
+ * Zademonstruj działanie tej klasy w programie, który wyświetla pytanie o numer przedziału i informację o tym, czy dany
+ * przedział jest zielony, czerwony czy czarny. Program powinien wyświetlać komunikat o błędzie, jeśli użytkownik
+ * wprowadzi numer z przedziału od 0 do 36.
+ */
+public class RoulettePocket {
+}
